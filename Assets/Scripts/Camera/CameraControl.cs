941437f54e9807e187abe04786cbad8aea659b4d
Nevermore1984 <15962976606@163.com>
2019-09-15 16:02:56 +0800

    Nevermore

---
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform heroPos;
+
+    private Vector3 offset;
+
+    private Vector3 v = Vector3.zero;
+
+    private Vector3 plus = Vector3.zero;
+
+    public GameObject hero;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - heroPos.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+    
+
+    private void move()
+    {
+        
+        plus = Vector3.zero;
+        if (hero.GetComponent<PlayerControl>().horizontalMove > 0)
+            plus += new Vector3(0.5f, 0, 0);
+        if (hero.GetComponent<PlayerControl>().horizontalMove < 0)
+            plus += new Vector3(-0.5f, 0, 0);
+        if (hero.GetComponent<PlayerControl>().verticalMove > 0)
+            plus += new Vector3(0, 0.5f, 0);
+        if (hero.GetComponent<PlayerControl>().verticalMove < 0)
+            plus += new Vector3(0, -0.5f, 0);
+
+        transform.position = Vector3.Lerp(transform.position, offset + heroPos.position + plus, 0.05f);
+    }
+}
---
+++ Assets/Scripts/Enemy/Doudou.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doudou : MonoBehaviour
+{
+    public GameObject player;
+    //仇恨范围
+    public float scopeOfHatred;
+    //移动速度
+    public float moveSpeed;
+
+    public Animator animator;
+
+    private bool direction=true;
+    private bool tempDiraction = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Hunt();
+    }
+
+    private void Hunt()
+    {
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < scopeOfHatred)
+        {
+            animator.SetBool("Move", true);
+            Vector3 dir = (player.transform.position - transform.position).normalized;
+            if(dir.x<0)
+            {
+                animator.SetBool("Direction", true);
+            }
+            else
+            {
+                animator.SetBool("Direction", false);
+            }
+            transform.Translate(dir * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("Move", false);
+        }
+    }
+}
